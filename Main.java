6c32e2673c4d927beb12c4bb1142cca801f53c08
Pablo-g-m2006 <pgarciaminguillan@alu.fpsanatonio.com>
2025-05-24 16:01:21 +0200

    Buenas tardes

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        double precioProducto1 = 10.5;
-        double precioProducto2 = 25.3;
-
-        double total = calcularPrecioTotal(precioProducto1, precioProducto2);
-
-        System.out.println("El precio total es: " + total);
+        double[] precios = {10.5, 25.3};
+        System.out.printf("El precio total es: %.2f%n", calcularPrecioTotal(precios));
     }
 
-    public static double calcularPrecioTotal(double precio1, double precio2) {
-        return precio1 + precio2;
+    private static double calcularPrecioTotal(double[] precios) {
+        double suma = 0;
+        for (double precio : precios) {
+            suma += precio;
+        }
+        return suma;
     }
 }
